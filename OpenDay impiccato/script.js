066849669f65e60be07bacd106d757767fe1a505
sickpoitew <109254578+Poitew@users.noreply.github.com>
2023-12-21 00:28:32 +0100

    Add files via upload

--- OpenDay impiccato/script.js
+++ OpenDay impiccato/script.js
@@ -1,21 +1,64 @@
-const parola = document.querySelector("#parola");
+const trattini = document.querySelector("#trattini");
+const immagine = document.getElementById("impiccato");
+const lettereSbagliate = document.getElementById("lettereSbagliate");
+let letteraGiusta;
 let arr = [];
+let arrTrattini = [];
+let conta = 0;
 
 function memorizzaValore() {
     const inputVal = document.getElementById("inputParola").value;
     arr = [...inputVal];
+    arrTrattini = new Array(arr.length).fill('_'); // crea un nuovo array di trattini con la lunghezza della parola inserita
     generaTrattini();
 }
 
 function generaTrattini(){
-    parola.innerHTML = " ";
+    trattini.innerHTML = arrTrattini.join(''); // fa comparire i trattini
     document.getElementById("inputParola").value = "";
+}
+
+function indovinaLettera(){
+    const letteraVal = document.getElementById("inputLettera").value;
+    document.getElementById("inputLettera").value = "";
+    letteraGiusta = false;
     for(let i = 0; i < arr.length; i++){
-        if(arr[i] === ' '){
-            parola.innerHTML += " ";
+        if(letteraVal === arr[i]){
+            arrTrattini[i] = letteraVal;
+            letteraGiusta = true;
+        }   
+    }
+    if(!letteraGiusta){    
+        lettereSbagliate.innerHTML += letteraVal + " ";
+        if(conta >= 6){
+            immagine.setAttribute("src", "image/impiccato/impiccato1.png");
+            arrTrattini = new Array(arr.length).fill(" ");
+            lettereSbagliate.innerHTML = " ";
+            generaTrattini();
         }
         else {
-            parola.innerHTML += "_";
+            switch (conta) {
+                case 0:
+                    immagine.setAttribute("src", "image/impiccato/impiccato2.png");
+                    break;
+                case 1:
+                    immagine.setAttribute("src", "image/impiccato/impiccato3.png");
+                    break;
+                case 2:
+                    immagine.setAttribute("src", "image/impiccato/impiccato4.png");
+                    break;
+                case 3:
+                    immagine.setAttribute("src", "image/impiccato/impiccato5.png");
+                    break;
+                case 4:
+                    immagine.setAttribute("src", "image/impiccato/impiccato6.png");
+                    break;
+                case 5:
+                    immagine.setAttribute("src", "image/impiccato/impiccato7.png");
+                    break;
+            }
         }
+        conta++;
     }
+    generaTrattini();
 }
